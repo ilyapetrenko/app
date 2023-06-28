30c2d9351597b11075c7b400f9ad1bba219a7d32
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-06-29 00:39:22 +0300

    +error middleware

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,12 +4,13 @@ const uuid = require('uuid')
 const mailService = require('./mail-service')
 const tokenService = require('./token-service')
 const UserDto = require('../dtos/user-dto')
+const ApiError = require('../exceptions/api-error')
 
 class UserService {
     async registration(email, password){
         const candidate = await UserModel.findOne({email})
         if (candidate){
-            throw new Error(`User with this email ${email} already exists`)
+            throw ApiError.BadRequest(`User with this email ${email} already exists`)
         }
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4()
@@ -30,7 +31,7 @@ class UserService {
     async activate(activationLink) {
         const user = await UserModel.findOne({activationLink})
         if (!user) {
-            throw new Error('Incorrect activation link')
+            throw ApiError.BadRequest('Incorrect activation link')
         }
         user.isActivated = true
         await user.save()
